91a1cbfc18b42956c76b25335b40f0fed90f4d36
Navamvg <725417@pdsb.net>
2021-11-27 15:25:21 -0500

    added println

--- Question3.java
+++ Question3.java
@@ -1,1 +1,10 @@
-//testing comment
+public class Question3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("SOS Ashmit and Zain can't pull shite");
+    }
+}
+    
+
+    //testing comment
