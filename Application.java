4a9d0c19a58a86aff30c022811226ecc370e97ec
Ashmit Mittal <653097@pdsb.net>
2021-11-29 18:23:47 -0500

    Created Check Balance method

--- Application.java
+++ Application.java
@@ -6,24 +6,28 @@ public class Application {
 
     // Constructor
     Application(String name, int bal) {
-        
+
         this.name = name;
         this.bal = bal;
 
     }
 
-    public void Withdraw(int num){
+    public void Withdraw(int num) {
 
         System.out.println("You withdrew $" + num + ". Your new balance is now $" + (bal - num));
         this.bal = this.bal - num;
 
     }
 
-    public void Deposit(int num){
-        
+    public void Deposit(int num) {
+
         System.out.println("You deposited $" + num + ". Your new balance is now $" + (bal + num));
         this.bal = this.bal + num;
 
+    }
+
+    public void CheckBal() {
+        System.out.println("Your current balance is $" + this.bal);
     }
 
 }
