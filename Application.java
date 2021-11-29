fdf6f9a10f0dd7a79b3c8c72411bd160475cbc1f
Ashmit Mittal <653097@pdsb.net>
2021-11-29 17:58:22 -0500

    Added constructor

--- Application.java
+++ Application.java
@@ -1,5 +1,13 @@
 public class Application {
 
+    // Variables
+    String name;
     int bal;
+
+    // Constructor
+    Application(String name, int bal) {
+        this.name = name;
+        this.bal = bal;
+    }
 
 }
